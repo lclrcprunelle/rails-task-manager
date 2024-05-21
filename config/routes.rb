e8d79888743693a74f5096c6b64722edc32164e2
Prunelle Leclerc <prunellelclrc@gmail.com>
2024-05-21 15:39:03 +0200

    Exo 2

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "list", to: "task"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id/destroy", to: "tasks#destroy", as: :destroy
+  delete "tasks/:id", to: "tasks#destroy"
 end
